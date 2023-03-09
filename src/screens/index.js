800aa6f3c27915681ec1066b7c3bdf1d6486a95e
minjun06 <35772631+minjun06@users.noreply.github.com>
2023-03-10 06:31:42 +0900

    Reverting to f22d869

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,6 +2,5 @@ import Home from './Home';
 import EnterName from './EnterName';
 import List from './List';
 import Chat from './Chat';
-import VideoPage from './VideoPage';
 
-export { Home, EnterName, List, Chat, VideoPage };
+export { Home, EnterName, List, Chat };
